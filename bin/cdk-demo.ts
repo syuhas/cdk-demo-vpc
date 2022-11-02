c00c6539584916ef3a81dbda4d7d1d12514621df
syuhas <syuhas22@gmail.com>
2022-11-01 23:54:51 -0400

    created VPC and subnets for the CDK demo

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -5,7 +5,24 @@ import { CdkDemoStack } from '../lib/cdk-demo-stack';
 
 const app = new cdk.App();
 new CdkDemoStack(app, 'CdkDemoStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
+
+    
+  env: { account: '551796573889', region: 'us-east-1' },
+
+
+  
+});
+
+
+
+
+
+
+
+
+
+
+/* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
@@ -18,4 +35,3 @@ new CdkDemoStack(app, 'CdkDemoStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new ec2.Vpc(this, 'myDemoVPC', {
+      
+      maxAzs: 3,
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      subnetConfiguration: [
+        
+        {
+          cidrMask: 24,
+          name: 'demoPublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+
+      ]
+    });
   }
 }
